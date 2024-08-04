2d235233eb96090df43186f80cc5697a69100e57
zhu-xinyu857 <19947709021@163.com>
2024-08-04 21:01:16 +0800

    240803

---
+++ 240803_priority_queue/240803_priority_queue/priority_queue.h
@@ -0,0 +1,85 @@
+#pragma once
+//模拟实现priority_queue类，并完成测试
+template<class T>
+class less {
+public:
+	operator()(T& x, T& y) {
+		return x < y;
+	}
+};
+
+template<class T>
+class greater {
+public:
+	operator()(T& x, T& y) {
+		return x > y;
+	}
+};
+
+namespace zxy
+{
+#include<vector>
+#include<functional>
+	template <class T, class Container = std::vector<T>, class Compare = less<T> >
+	class priority_queue
+	{
+	public:
+		void AjustUp(T* a, int child) {
+			Compare com;
+			int parent = (child - 1) / 2;
+			while (child > 0) {
+				if (com(a[parent], a[child])) {
+					Swap(&a[child], &a[parent]);
+					child = parent;
+					parent = (child - 1) / 2;
+				}
+				else {
+					break;
+				}
+			}
+		}
+
+		void AjustDown(T* a, int size, int parent) {
+			int child = parent * 2 + 1;
+			while (child < size) {
+				if (child + 1 < size && com(a[child]), a[child + 1]) {
+					child++;
+				}
+				if (com(a[parent], a[child])) {
+					Swap(&a[child], &a[parent]);
+					parent = child;
+					child = parent * 2 + 1;
+				}
+				else {
+					break;
+				}
+			}
+		}
+		priority_queue(){}
+
+		template <class InputIterator>
+		priority_queue(InputIterator first, InputIterator last) {
+			auto i = first;
+			while (i != last) {
+				push(*i);
+				i++;
+			}
+		}
+
+		bool empty() const {return c.empty();}
+		size_t size() const {return c.size();}
+		T& top() const {return c.front();}
+
+		void push(const T& x) {
+			c.push(x);
+			AjustUp(c, c.size() - 1);
+		}
+		void pop() {
+			swap(c.front(), c.back());
+			AjustDown(c, c.size() - 1, 0);
+		}
+	private:
+		Container c;
+		Compare comp;
+	};
+};
---
+++ 240803_stack&queue/240803_stack&queue/stack&queue.h
@@ -0,0 +1,63 @@
+#pragma once
+//模拟实现stack类和queue类，并完成测试
+#include<deque>
+namespace zxy{
+	template<class T, class Con = std::deque<T>>
+	class stack
+	{
+	public:
+		stack() {}
+		void push(const T& x) {
+			_c.push_back(x);
+		}
+		void pop() {
+			_c.pop_back();
+		}
+		T& top() {
+			return _c.back();
+		}
+		const T& top()const {
+			return _c.back();
+		}
+		size_t size()const {
+			return _c.size();
+		}
+		bool empty()const {
+			return _c.empty();
+		}
+	private:
+		Con _c;
+	};
+	template<class T, class Con = std::deque<T>>
+	class queue
+	{
+	public:
+		queue(){}
+		void push(const T& x) {
+			_c.push_back(x);
+		}
+		void pop() {
+			_c.pop_front();
+		}
+		T& back() {
+			return _c.back();
+		}
+		const T& back()const {
+			return _c.back();
+		}
+		T& front() {
+			return _c.front();
+		}
+		const T& front()const {
+			return _c.front();
+		}
+		size_t size()const {
+			return _c.size();
+		}
+		bool empty()const {
+			return _c.empty();
+		}
+	private:
+		Con _c;
+	};
+};
